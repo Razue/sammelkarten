1dcf57393fb10ff1ba3facf204bc576389709328
Razue <86399543+Razue@users.noreply.github.com>
2025-08-03 13:08:54 +0200

    fix test

--- test/sammelkarten_web/controllers/page_controller_test.exs
+++ test/sammelkarten_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SammelkartenWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Sammelkarten"
   end
 end
